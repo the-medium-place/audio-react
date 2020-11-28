8086945ac40a46d077894e7293a48b4debb4428d
ZGS <zgstowell@gmail.com>
2020-11-28 13:28:22 -0800

    changed to deploy vars

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+// const API_URL = 'http://localhost:8080';
+const API_URL = 'https://zgstowell-audiology-api.herokuapp.com';
 
 const API = {
 
